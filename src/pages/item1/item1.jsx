962f881bdcbe5d110657ff49b65709b55676839c
labnize <zhulixh@126.com>
2017-08-01 14:25:52 +0800

    代码格式化

--- src/pages/item1/item1.jsx
+++ src/pages/item1/item1.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable class-methods-use-this */
 import React, { Component } from 'react';
 import { Table, Input, Button } from 'antd';
 import Tablestore from 'store/tablestore';
--- src/pages/item1/userconfig.jsx
+++ src/pages/item1/userconfig.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { Form, Input, Radio, Button } from 'antd';
+import modal from 'components/modal/modal';
+import Ajax from 'util/ajax';
 import './userConfig.less';
 
 const FormItem = Form.Item;
 const { TextArea } = Input;
+const url = 'claa/createuser';
 
 class PageComponent extends Component {
+  static cancelClickHandler() {
+    modal.closeModel();
+  }
   constructor() {
     super();
     this.state = {
@@ -13,51 +19,89 @@ class PageComponent extends Component {
     };
   }
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+        const param = {
+          loadingFlag: true,
+          url,
+          method: 'POST',
+          data: {
+            username: values.username,
+            rolename: values.rolename,
+            userDesc: values.userDesc
+          },
+          successFn() {
+
+          }
+        };
+        Ajax.fetch(param);
+      }
+    });
+    PageComponent.cancelClickHandler();
+  };
+
   render() {
     const { formLayout } = this.state;
-    const formItemLayout = formLayout === 'horizontal' ? {
+    const formItemLayout = {
       labelCol: { span: 5 },
       wrapperCol: { span: 16 }
-    } : null;
+    };
+    const { getFieldDecorator } = this.props.form;
     return (
       <div >
-        <Form layout={formLayout} className="userConfig">
+        <Form onSubmit={this.handleSubmit} layout={formLayout} className="userConfig" >
           <FormItem
             label="用户类型"
             {...formItemLayout}
           >
-            <Radio.Group defaultValue="horizontal" onChange={this.handleFormLayoutChange} >
-              <Radio.Button value="普通用户" >普通用户</Radio.Button >
-              <Radio.Button value="管理员" >管理员</Radio.Button >
-            </Radio.Group >
+            {getFieldDecorator('rolename', {
+              rules: [{ required: true, message: '请选择用户类型!' }]
+            })(
+              <Radio.Group>
+                <Radio.Button value="普通用户" >普通用户</Radio.Button >
+                <Radio.Button value="管理员" >管理员</Radio.Button >
+              </Radio.Group >
+            )}
           </FormItem >
           <FormItem
             label="用户名"
             {...formItemLayout}
           >
-            <Input placeholder="请填写用户名" />
+            {getFieldDecorator('username', {
+              rules: [{ required: true, message: '请输入用户名!' }]
+            })(
+              <Input placeholder="请填写用户名" />
+            )}
           </FormItem >
           <FormItem
             label="用户描述"
             {...formItemLayout}
           >
-            <TextArea rows={4} />
+            {getFieldDecorator('userDesc', {
+              rules: [{ required: true, message: '请输入用户描述!' }]
+            })(
+              <TextArea rows={4} />
+            )}
           </FormItem >
           <FormItem
             wrapperCol={{ span: 24 }}
             className="footer"
           >
-            <Button type="primary" htmlType="submit">
+            <Button type="primary" htmlType="submit" >
               确定
-            </Button>
-            <Button style={{ marginLeft: 8 }}>
+            </Button >
+            <Button style={{ marginLeft: 8 }} onClick={PageComponent.cancelClickHandler} >
               取消
-            </Button>
-          </FormItem>
+            </Button >
+          </FormItem >
         </Form >
       </div >
     );
   }
 }
 
-export default PageComponent;
+const WrappedNormalLoginForm = Form.create()(PageComponent);
+export default WrappedNormalLoginForm;
