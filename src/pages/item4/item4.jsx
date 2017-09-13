c25f2c42e2942571effeab81bf333baf7ec247a7
labnize <zhulixh@126.com>
2017-09-13 15:17:26 +0800

    amap添加markers

--- src/pages/item4/item4.jsx
+++ src/pages/item4/item4.jsx
@@ -1,28 +1,40 @@
 import React, { Component } from 'react';
 import Layout from 'components/layout2/layout2';
-import { Map, GroundImage } from 'react-amap';
+import { Map, GroundImage, Markers } from 'react-amap';
 import srcImg from 'images/dongwuyuan.jpg';
 import './item4.less';
-
-const bounds = {
-  sw: {
-    longitude: 116.327911,
-    latitude: 39.939229
-  },
-  ne: {
-    longitude: 116.342659,
-    latitude: 39.946275
-  }
-};
-
-const center = {
-  longitude: 116.33719,
-  latitude: 39.942384
-};
 
 class PageComponent extends Component {
   constructor(props) {
     super(props);
+    this.center = {
+      longitude: 116.33719,
+      latitude: 39.942384
+    };
+    this.markers = [
+      {
+        position: {
+          longitude: 116.333124,
+          latitude: 39.941849
+        }
+      },
+      {
+        position: {
+          longitude: 116.340934,
+          latitude: 39.944794
+        }
+      }
+    ];
+    this.bounds = {
+      sw: {
+        longitude: 116.327911,
+        latitude: 39.939229
+      },
+      ne: {
+        longitude: 116.342659,
+        latitude: 39.946275
+      }
+    };
   }
 
   render() {
@@ -32,14 +44,17 @@ class PageComponent extends Component {
           <div id="container">
             <Map
               plugins={['ToolBar']}
-              center={center}
+              center={this.center}
               zoom={15}
-              zooms={[15, 20]}
+              zooms={[12, 20]}
               expandZoomRange
               resizeEnable
             >
+              <Markers
+                markers={this.markers}
+              />
               <GroundImage
-                bounds={bounds}
+                bounds={this.bounds}
                 src={srcImg}
               />
             </Map>
