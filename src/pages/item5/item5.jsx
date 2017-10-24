792a4995b90f5b597811c6e5c6dddbcaa1b50805
labnize <zhulixh@126.com>
2017-10-24 15:01:55 +0800

    安装vis，并添加拓扑图demo

--- src/pages/item5/item5.jsx
+++ src/pages/item5/item5.jsx
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import Layout from 'components/layout2/layout2';
+import vis from 'vis';
+import './item5.less';
 
 class PageComponent extends Component {
+  componentDidMount() {
+    const container = document.getElementById('mynetwork');
+    const nodes = new vis.DataSet([
+      { id: 1, label: 'Node 1' },
+      { id: 2, label: 'Node 2' },
+      { id: 3, label: 'Node 3' },
+      { id: 4, label: 'Node 4' },
+      { id: 5, label: 'Node 5' }
+    ]);
+    const edges = new vis.DataSet([
+      { from: 1, to: 3 },
+      { from: 1, to: 2 },
+      { from: 2, to: 4 },
+      { from: 2, to: 5 }
+    ]);
+
+    const data = {
+      nodes,
+      edges
+    };
+    const options = {};
+    const network = new vis.Network(container, data, options);
+  }
   render() {
     return (
       <Layout name="item5">
-        <div></div>
+        <div id="mynetwork" />
       </Layout>
     );
   }
