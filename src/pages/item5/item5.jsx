6ed09e4e711ae8e57b0b3284cdc2b13b374ed69c
labnize <zhulixh@126.com>
2017-11-15 14:58:44 +0800

    代码格式化

--- src/pages/item5/item5.jsx
+++ src/pages/item5/item5.jsx
@@ -9,15 +9,26 @@ class PageComponent extends Component {
   componentDidMount() {
     const container = document.getElementById('mynetwork');
     const nodes = new DataSet([
-      { id: 1, label: '网关1', shape: 'image', image: `${nodeImage}` },
-      { id: 2, label: '网关2', shape: 'image', image: `${nodeImage}` },
-      { id: 3, label: '网关3', shape: 'image', image: `${nodeImage}` },
-      { id: 4, label: '网关4', shape: 'image', image: `${nodeImage}` },
-      { id: 5, label: '网关5', shape: 'image', image: `${nodeImage}` }
+      {
+        id: 1, label: '网关1', shape: 'image', image: `${nodeImage}`
+      },
+      {
+        id: 2, label: '网关2', shape: 'image', image: `${nodeImage}`
+      },
+      {
+        id: 3, label: '网关3', shape: 'image', image: `${nodeImage}`
+      },
+      {
+        id: 4, label: '网关4', shape: 'image', image: `${nodeImage}`
+      },
+      {
+        id: 5, label: '网关5', shape: 'image', image: `${nodeImage}`
+      }
     ]);
     const edges = new DataSet([
       { from: 1, to: 3, dashes: true },
-      { from: 1,
+      {
+        from: 1,
         to: 2,
         color: {
           color: 'red',
