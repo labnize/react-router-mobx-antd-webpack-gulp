b73cf1829cc380757b516b5a31bec14cba6a9a37
labnize <zhulixh@126.com>
2017-12-16 11:53:44 +0800

    antd默认语言设置为中文

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import zhCN from 'antd/lib/locale-provider/zh_CN';
+import { LocaleProvider } from 'antd';
 import App from './app';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <LocaleProvider locale={zhCN}>
+      <App />
+    </LocaleProvider>
   </BrowserRouter>,
   document.getElementById('app')
 );
